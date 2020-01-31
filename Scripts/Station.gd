ea6a181c3ab88fe2eeb6c503aa781fd26fa11724
Tomasz Chabora <kobewi4e@gmail.com>
2020-01-31 22:29:20 +0100

    Jakaś porażka

--- Scripts/Station.gd
+++ Scripts/Station.gd
@@ -4,5 +4,6 @@ onready var player := $Player as KinematicBody
 onready var camera := $Camera as Camera
 
 func _process(delta: float) -> void:
-	camera.translation = player.translation - player.z_vector() * 5 + player.up * 5
-	camera.rotation.z = player.rotation.z
+	camera.translation = player.translation - player.z_vector() * 5 + player.up * 1
+	camera.look_at(player.translation, player.up)
+#	camera.rotation.z = player.rotation.z
