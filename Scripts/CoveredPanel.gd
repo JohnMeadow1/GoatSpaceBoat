e74d57be5e4f48aef2c26d772123328c0dc181ba
Tomasz Chabora <kobewi4e@gmail.com>
2020-02-01 03:41:33 +0100

    Kurde śrubki

--- Scripts/CoveredPanel.gd
+++ Scripts/CoveredPanel.gd
@@ -3,6 +3,7 @@ extends Sprite
 export var underpanel: PackedScene
 
 var screws: int
+var fall: float
 
 func _ready() -> void:
 	for node in get_children():
@@ -10,8 +11,13 @@ func _ready() -> void:
 			screws += 1
 			node.connect("finished", self, "screw_end")
 
+func _physics_process(delta: float) -> void:
+	if fall:
+		fall += delta * 10
+		position.y += fall
+
 func screw_end():
 	screws -= 1
 	if screws == 0:
 		get_parent().add_child(underpanel.instance())
-		queue_free()
+		fall = 1
