afb1eeb88282a91bdbe92dd425bed7039a9599ae
Tomasz Chabora <kobewi4e@gmail.com>
2020-02-02 03:41:40 +0100

    Wyłączanie silnika czy coś

--- Scripts/PanelDisplay.gd
+++ Scripts/PanelDisplay.gd
@@ -52,11 +52,7 @@ func _input(event: InputEvent) -> void:
 	if focused and not tween.is_active():
 		if event is InputEventKey:
 			if event.pressed and event.scancode == KEY_ESCAPE:
-				tween.interpolate_property(cam, "global_transform:origin", camera_target.global_transform.origin, camera_origin.origin, 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-				tween.interpolate_property(cam, "global_transform:basis", camera_target.global_transform.basis, camera_origin.basis, 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-				tween.start()
-				focused = false
-				Singleton.player_locked = false
+				exit()
 
 func set_size(s: Vector2):
 	size = s
@@ -73,3 +69,10 @@ func anim_end():
 		panel.propagate_call("panel_focus")
 	else:
 		panel.propagate_call("panel_unfocus")
+
+func exit():
+	tween.interpolate_property(cam, "global_transform:origin", camera_target.global_transform.origin, camera_origin.origin, 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.interpolate_property(cam, "global_transform:basis", camera_target.global_transform.basis, camera_origin.basis, 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
+	focused = false
+	Singleton.player_locked = false
